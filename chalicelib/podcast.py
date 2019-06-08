ca05487d25bf9912032db02a7c869958bbd6946f
Kyle Polich <kyle@dataskeptic.com>
2019-06-08 12:14:48 -0700

    bug fix

--- chalicelib/podcast.py
+++ chalicelib/podcast.py
@@ -69,6 +69,6 @@ def update_episode(database, episode):
             return []
     else:
         # TODO: if episode is more than N minutes old, email Kyle once
-        print(f"********************* --- WTF is {prettyname}")
+        print("********************* --- WTF is {prettyname}".format(prettyname=prettyname))
         return []
 
